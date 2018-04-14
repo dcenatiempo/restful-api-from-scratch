5d67f2bf9e5c8604cc15c461f419b0b382ee31dd
Devin Cenatiempo <dcenatiempo@gmail.com>
2018-04-14 08:54:11 -0600

    Added workers file, refactored index.js

--- index.js
+++ index.js
@@ -1,83 +1,14 @@
-// This is the primary file for the API
-
 // Dependencies
-const http = require('http');
-const https = require('https')
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./lib/config');
-const fs = require('fs');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-// Instantiating the http server
-var httpServer = http.createServer( (req, res) => {
-  unifiedServer(req, res);
-});
-// Start the http server
-httpServer.listen(config.httpPort, () => {
-  console.log(`The server is listening on port ${config.httpPort} in ${config.envName} mode`);
-});
-// Instantiating the http server
-var httpsServerOptions = {
-  'key': fs.readFileSync('./https/key.pem'),
-  'cert': fs.readFileSync('./https/cert.pem')
-};
-var httpsServer = https.createServer(httpsServerOptions, (req, res) => {
-  unifiedServer(req, res);
-});
-// Start the http server
-httpsServer.listen(config.httpsPort, () => {
-  console.log(`The server is listening on port ${config.httpsPort} in ${config.envName} mode`);
-});
+var app = {};
 
-
-var unifiedServer = (req, res) => {
-  var parsedUrl = url.parse(req.url, true);
-  var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/$/g, '');
-
-  var queryStringObject = parsedUrl.query;
-
-  var method = req.method.toLowerCase();
-
-  var headers = req.headers;
-
-  var decoder = new StringDecoder('utf-8');
-  var buffer = '';
-  req.on('data', data => {
-    buffer += decoder.write(data);
-  });
-  req.on('end', () => {
-    buffer += decoder.end();
-
-    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-    var data = {
-      'trimmed path': trimmedPath,
-      'queryStringObject': queryStringObject,
-      'method': method,
-      'headers': headers,
-      'payload': helpers.parseJsonToObject(buffer)
-    };
-
-    chosenHandler(data, (statusCode, payload) => {
-      statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
-      payload = typeof(payload) === 'object' ? payload : {};
-      var payloadString = JSON.stringify(payload);
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(statusCode);
-      res.end(`${payloadString}\n`);
-      console.log('Returning this response: ' + statusCode, payloadString);
-    });
-
-  });
-  console.log(headers);
+app.init = () => {
+  server.init();
+  workers.init();
 };
 
-var router = {
-  'ping': handlers.ping,
-  'users': handlers.users,
-  'tokens': handlers.tokens,
-  'checks': handlers.checks
-}
+app.init();
+
+module.exports = app;
