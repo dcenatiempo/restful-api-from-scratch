f23f4d289824a0e17e7a96ce4daa9da7df53c1e4
Devin Cenatiempo <dcenatiempo@gmail.com>
2018-04-18 22:07:58 -0600

    Add a simple CLI admin tool

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Dependencies
 const server = require('./lib/server');
 const workers = require('./lib/workers');
+const cli = require('./lib/cli');
 
 var app = {};
 
 app.init = () => {
   server.init();
   workers.init();
+  setTimeout( () => {
+    cli.init();
+  }, 50);
 };
 
 app.init();
